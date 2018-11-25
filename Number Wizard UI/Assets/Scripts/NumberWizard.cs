d996c10e1f8a6547e36dc8fec1a8c339938c4f11
William Howell <whowell@mente-tattica.com>
2018-11-24 22:19:44 -0500

    Initialize Repo

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+
+    // Decalare the instance variables
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessValue;
+    int guess; 
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+        StartGame();
+  
+    }
+
+    void StartGame ()
+    {
+        // Reset Values for each new game.
+
+        GuessGenerator();
+        
+
+    }
+
+    void GuessGenerator()
+    {
+        guess = Random.Range(min, max + 1);
+        guessValue.text = guess.ToString();
+    }
+
+    void NextGuess()
+    {
+        GuessGenerator();
+
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    
+}
---
+++ Number Wizard UI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	// Utility Script to navigate through the scenes
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
